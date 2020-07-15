d9ffe0f02294716e7273dc51a7844e7d2cd76ea2
Ankit Kumar <ankitamuniverse@gmail.com>
2020-07-15 23:21:32 +0530

    Add files via upload

--- js/blog.js
+++ js/blog.js
@@ -6,6 +6,24 @@
     var blogWrapper = document.querySelector("#blog .blog-wrapper");
 
     var content = [{
+        heading: "The New CORONA Way Of Life",
+        description: "During the pandemic, technology is what keeps us still together and functional in an increasingly disconnected and chaotic world and this growth may have a long-lasting impact on a post-COVID world. I have discussed about the same and new technology trends.",
+        link: "https://iiitg-blogs.com/2020/07/13/the-curriculum-vitae-the-cv/7/",
+        date: {
+            dd: 15,
+            month: 07,
+            year: 2020
+        }
+    },{
+        heading: "The New Industrial Revolution",
+        description: "I have talked about how Coronavirus Accelerated the 4th Industrial Revolution.",
+        link: "https://iiitg-blogs.com/2020/07/13/the-curriculum-vitae-the-cv/7/",
+        date: {
+            dd: 15,
+            month: 07,
+            year: 2020
+        }
+    },{
         heading: "How To Make Text Typing Effect?",
         description: "Wrote a step-by-step guide to mimic the typing effect using JavaScript. Please give it a read!",
         link: "https://medium.com/web-for-you/how-to-make-typing-effect-c72d9d6b9772",
@@ -34,12 +52,11 @@
         }
     }];
 
-    var backgroundColors = ["rgb(204, 255, 204)","rgb(204, 204, 255)","rgb(255, 255, 204)"];
+    var backgroundColors = ["rgb(255, 190, 11)","rgb(239, 111, 108)","rgb(12, 164, 165)","rgb(132, 195, 24)"];
 
     var i,temp,title,descriptionPara,date,randNum,tempLink;
 
     for(i = 0; i<content.length ; i++) {
-        
         //Creation Stage
         temp = document.createElement("div");
         title = document.createElement("h1");
@@ -50,7 +67,7 @@
         descriptionPara.textContent = content[i].description;
         date.textContent = content[i].date.dd + "." + content[i].date.month + "." + content[i].date.year;
         //Setting background colors
-        temp.style.backgroundColor = backgroundColors[Math.floor((Math.random()*10)+1)%3];
+        temp.style.backgroundColor = backgroundColors[Math.floor((Math.random()*10)+1)%(backgroundColors.length)];
         //Rotating Slightly
         randNum = ((Math.floor(Math.random()*10)+1)%8);
         if(randNum % 2 === 0) {
