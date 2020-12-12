eb4d281cb41537ed005c81f0d4b63909ca5f3eb5
Ankit Kumar <kumar.ankit.862001@gmail.com>
2020-12-12 21:05:29 +0530

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,1 +1,4 @@
-function onReady(a){var b=window.setInterval(function(){void 0!==document.getElementsByTagName("body")[0]&&(window.clearInterval(b),a.call(this))},1E3)}function setVisible(a,b){document.querySelector(a).style.display=b?"block":"none"}onReady(function(){setVisible("body",!0);setVisible("#loading-screen",!1)});
+window.onload = function() {
+  const loader = document.getElementById("loading-screen");
+  loader.style.display = "none";
+};
